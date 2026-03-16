2fe2d204713f4babd804e96baf88c05334fe821e
Doroteu Soares Ribeiro Neto <doroteuneto1@gmail.com>
2026-03-16 18:17:41 -0300

    test: isolando analise de vulnerabilidades de bibliotecas reais

--- config/src/login.py
+++ config/src/login.py
@@ -6,6 +6,3 @@ def verificar_login(usuario, senha):
     query = "SELECT * FROM usuarios WHERE username = ? AND password = ?"
     cursor.execute(query, (usuario, senha))
     return cursor.fetchone()
-# TESTE DE SEGURANÇA: ESTA LINHA DEVE SER BLOQUEADA PELA ESTEIRA
-AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
-AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEYp
