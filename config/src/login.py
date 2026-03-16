992a73bb4c347a8d84a1a8380b3672b4c41294cb
Doroteu Soares Ribeiro Neto <doroteuneto1@gmail.com>
2026-03-16 17:31:31 -0300

    test: simulando vazamento de senha para validar Gitleaks

--- config/src/login.py
+++ config/src/login.py
@@ -6,3 +6,5 @@ def verificar_login(usuario, senha):
     query = "SELECT * FROM usuarios WHERE username = ? AND password = ?"
     cursor.execute(query, (usuario, senha))
     return cursor.fetchone()
+# Configuração de teste
+ADMIN_PASSWORD = "minha_senha_super_secreta_123"
